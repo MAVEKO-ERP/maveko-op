ae3a50afa8be314a221b23b647ad73fa16db258a
nigussolomon <nigussolomon444@gmail.com>
2023-08-08 23:34:50 +0300

    setup supplier creation form

--- src/pages/Supplier.jsx
+++ src/pages/Supplier.jsx
@@ -24,7 +24,20 @@ export default function Supplier() {
   const [openDialog, setOpenDialog] = useState(false);
   const [dialogActive, setDialogActive] = useState("1");
   const [supplierName, setSupplierName] = useState("");
-  
+  const [refNo, setRefNo] = useState("");
+  const [owner, setOwner] = useState("");
+  const [currency, setCurrency] = useState("1");
+  const [cusTitle, setCusTitle] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
+  const [addressLine, setAddressLine] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+  const [country, setCountry] = useState("");
+
   const inputFields = [
     {
       label: "Supplier Name",
@@ -32,7 +45,108 @@ export default function Supplier() {
       change: setSupplierName,
       width: "332.5px",
     },
-  ]
+    {
+      label: "Reference Number",
+      value: refNo,
+      change: setRefNo,
+      width: "332.5px",
+    },
+    {
+      label: "Owner",
+      value: owner,
+      change: setOwner,
+      width: "332.5px",
+      select: true,
+      options: [
+        { value: "1", label: "Hana Gebeyehu" },
+        { value: "2", label: "Tibebu Biru" },
+        { value: "3", label: "Nigus Solomon" },
+      ],
+    },
+    {
+      label: "Currency",
+      value: currency,
+      change: setCurrency,
+      width: "150px",
+      options: [
+        { value: "1", label: "EUR" },
+        { value: "2", label: "USD" },
+      ],
+    },
+  ];
+
+  const inputFieldsContact = [
+    {
+      label: "Title",
+      select: true,
+      value: cusTitle,
+      change: setCusTitle,
+      width: "132.5px",
+      options: [
+        { value: "1", label: "Mr" },
+        { value: "1", label: "Ms" },
+      ],
+    },
+    {
+      label: "First Name",
+      value: firstName,
+      change: setFirstName,
+      width: "332.5px",
+    },
+    {
+      label: "Last Name",
+      value: lastName,
+      change: setLastName,
+      width: "332.5px",
+    },
+    {
+      label: "Gender",
+      select: true,
+      value: gender,
+      change: setGender,
+      width: "132.5px",
+      options: [
+        { value: "1", label: "Male" },
+        { value: "1", label: "Female" },
+      ],
+    },
+    {
+      label: "Email",
+      value: email,
+      change: setEmail,
+      width: "332.5px",
+    },
+    {
+      label: "Address Line",
+      value: addressLine,
+      change: setAddressLine,
+      width: "332.5px",
+    },
+    {
+      label: "Country",
+      value: country,
+      change: setCountry,
+      width: "332.5px",
+    },
+    {
+      label: "City",
+      value: city,
+      change: setCity,
+      width: "332.5px",
+    },
+    {
+      label: "State/Province",
+      value: state,
+      change: setState,
+      width: "332.5px",
+    },
+    {
+      label: "Zip/Postal Code",
+      value: zip,
+      change: setZip,
+      width: "332.5px",
+    },
+  ];
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -111,7 +225,14 @@ export default function Supplier() {
             </TabPanel>
           </TabPanel>
           <TabPanel value="2">
-            <CreateForm inputFields={inputFields} formType="SAVE SUPPLIER"></CreateForm>
+            <CreateForm inputFields={inputFields} noActions></CreateForm>
+            <h2 style={{ marginLeft: "-18px" }}>Contact Details</h2>
+            <Divider style={{ marginLeft: "-18px" }}></Divider>
+            <br />
+            <CreateForm
+              inputFields={inputFieldsContact}
+              formType="SAVE SUPPLIER"
+            ></CreateForm>
           </TabPanel>
         </TabContext>
       </Box>
